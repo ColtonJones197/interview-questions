62b27ed22cfb5a8fdc9ada7cc45b6abb6d8f339d
Colton Jones <ckjones97@ksu.edu>
2022-10-02 20:21:47 -0500

    revised 2sum

--- arrays/1-two-sum.py
+++ arrays/1-two-sum.py
@@ -5,7 +5,8 @@ class Solution:
         # if target - the value that I'm currently on exists in keys, return the index that I'm currently on and the value at target - current val
         # so k: index of the element, v: target - value at element
         diffToIndexMap = {}
-        for index in range(0, len(nums)):
+        for index in range(0, len(nums)): # after looking at leetcode, the better way to write this would be using the enumerate function in python. This allows you to use a value for the index and a value for the actual value.
+            #would look like: for i, n in enumerate(nums):
             currentVal = nums[index]
             diff = target - currentVal
             if diff in diffToIndexMap.keys():
