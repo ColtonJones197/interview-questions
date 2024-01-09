95b5ab7fd4e4e41b1602f2de81af2b0fef37cac7
ColtonJones197 <ckjones97@ksu.edu>
2024-01-09 03:07:21 -0600

    valid palindrome

---
+++ two-pointers/125-valid-palindrome.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-palindrome/description/
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+            elif not s[end].isalnum():
+                end -= 1
+            elif s[start].lower() != s[end].lower():
+                return False
+            else:
+                start += 1
+                end -= 1
+        return True
